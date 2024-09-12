c4b746175e50597e0bf5630a0de5d54d9cfa41e5
taufiq <taufiq.elahi.a@gmail.com>
2024-09-12 14:33:00 +0600

    Andriod intent edited and static sim slection removed

--- lib/direct_caller_sim_choice.dart
+++ lib/direct_caller_sim_choice.dart
@@ -6,28 +6,23 @@ import 'package:android_intent_plus/android_intent.dart';
 /// A Class to make phone calls using the default dialer app.
 class DirectCaller {
   /// Makes a phone call using the default dialer app.
-  bool makePhoneCall(String phoneNumber, {int simSlot = 1}) {
+  bool makePhoneCall(String phoneNumber) {
     /// Check if the phone number is of 10 digits.
     // if (phoneNumber.length != 10) {
     //   throw Exception('Phone number must be of 10 digits');
     // }
-    if (simSlot != 1 && simSlot != 2) {
-      throw Exception('Sim slot must be 1 or 2');
-    }
+
 
     /// Check if the platform is Android.
-    if (Platform.isAndroid) {
+    if (Platform.isAndroid) { // For devices with Android Lollipop and above
       final intent = AndroidIntent(
         action: 'android.intent.action.CALL',
         data: 'tel:$phoneNumber',
-        arguments: {
-          'com.android.phone.force.slot': true,
-          'com.android.phone.extra.slot':
-              simSlot - 1, // slot index starts from 0
-        },
-      );
-      intent.launch();
+        package: 'com.android.phone',
 
+      );
+
+      intent.launch();
       return true;
     } else {
       return false;
